ad6d45dd1fdc262968aec0c5c385810adb686df9
vitorAlmeid <vitoraaalmeida@live.com>
2021-06-22 20:59:39 -0300

    feat: add error handler middleware directly on server

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import 'reflect-metadata';
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import "express-async-errors";
+
 import { router } from './routes'
 
 import './database';
@@ -8,5 +10,17 @@ const app = express();
 app.use(express.json());
 
 app.use(router);
+
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+    if (error instanceof Error) {
+        return res.status(400).json({ 
+            message: error.message
+        })
+    };
+
+    return res.status(500).json({
+        message: 'Internal Server Error.'
+    });
+});
 
 app.listen(3000, () => console.log('Server is running ;D'));
