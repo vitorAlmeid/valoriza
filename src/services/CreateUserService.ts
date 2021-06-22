40e564c903e079a7411e5cae91e08c7dc5c22d9b
vitorAlmeid <vitoraaalmeida@live.com>
2021-06-21 22:54:35 -0300

    feat: implemented CreateUserService

---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,33 @@
+import { getCustomRepository } from "typeorm";
+import { UserRepository } from "../repositories/UserRepository";
+
+interface IUserRequest {
+    name: string;
+    email: string;
+    admin?: boolean;
+}
+
+class CreateUserService {
+
+    async execute ({ name, email, admin } : IUserRequest) {
+        const userRepository = getCustomRepository(UserRepository);
+
+        if (!email) {
+            throw new Error("Missing params: e-mail");
+        };
+
+        const userAlreadyExists = await userRepository.findOne({ email });
+
+        if (userAlreadyExists) {
+            throw new Error("User already exists");
+        };
+
+        const user = userRepository.create({ name, email, admin });
+
+        await userRepository.save(user);
+
+        return user;
+    }
+}
+
+export { CreateUserService };
