21b2aa23fefe3abc84b5b3f1892f1c6e1d1e4b10
vitorAlmeid <vitoraaalmeida@live.com>
2021-06-24 21:41:14 -0300

    refactor: verify if user is admin

--- src/middlewares/ensureAdmin.ts
+++ src/middlewares/ensureAdmin.ts
@@ -1,10 +1,15 @@
 import {  Request, Response, NextFunction, response } from 'express';
+import { getCustomRepository } from 'typeorm';
+import { UserRepository } from '../repositories/UserRepository';
 
 
 
-export function ensureAdmin(req: Request, res: Response, next: NextFunction) {
-    const admin = false;
+export async function ensureAdmin(req: Request, res: Response, next: NextFunction) {
+    const userRepository = getCustomRepository(UserRepository);
+    const { user_id: id } = req;
 
+    const { admin } = await userRepository.findOne(id);
+    
     if (admin) {
         return next();
     };
