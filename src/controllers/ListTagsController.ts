ed2c7eb14ce43cf7eb545c9e98546c1cbcf72551
vitorAlmeid <vitoraaalmeida@live.com>
2021-06-24 22:38:16 -0300

    feat: create ListTagsController

---
+++ src/controllers/ListTagsController.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from "express";
+import { ListTagsService } from "../services/ListTagsService";
+
+class ListTagsController {
+    async handle(req: Request, res: Response) {
+        const listTagsService = new ListTagsService();
+
+        const tags = await listTagsService.execute();
+
+        return res.status(200).json(tags);
+    };
+};
+
+export { ListTagsController };
